f9db9307083d2efef79252a476c33cbb521bc504
Joaco <joaco.martinez1480@gmail.com>
2026-06-08 08:51:57 -0300

    3

--- backend/src/cron/index.ts
+++
@@ -1,6 +0,0 @@
-import { iniciarRenovacionAutomatica } from "./afipRenovador";
-
-(async () => {
-  console.log("🟡 Servicio de cron AFIP iniciado...");
-  await iniciarRenovacionAutomatica();
-})();
